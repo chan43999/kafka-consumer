916aafd319a1d708f0ec78f4cbe52ee92d2bd347
chansuttichujit <chan.suttichujit@ibm.com>
2022-01-20 10:20:58 +0700

    remove duplicate annotations

--- src/main/java/com/example/ktb/kafka/MyMessage.java
+++ src/main/java/com/example/ktb/kafka/MyMessage.java
@@ -3,12 +3,10 @@ package com.example.ktb.kafka;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class MyMessage {
 
     String name;
