5db8341a41aa81a2cce2174e8433776cb98287cc
chansuttichujit <chan.suttichujit@ibm.com>
2022-01-20 10:12:51 +0700

    add acknowledgement

--- src/main/java/com/example/ktb/kafka/Consumer.java
+++ src/main/java/com/example/ktb/kafka/Consumer.java
@@ -1,16 +1,18 @@
 package com.example.ktb.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class Consumer {
 
     @KafkaListener(topics = "test", containerFactory = "kafkaListenerContainerFactory")
     public void listen(@Payload MyMessage message, Acknowledgment acknowledgment){
-        System.out.println(message);
+        log.info(message.toString());
         acknowledgment.acknowledge();
     }
 }
