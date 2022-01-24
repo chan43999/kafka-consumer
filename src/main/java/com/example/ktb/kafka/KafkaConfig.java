faa3178e1755a3c08609938ac3948819be4427c6
chansuttichujit <chan.suttichujit@ibm.com>
2022-01-24 14:19:32 +0700

    configure consumer with built in properties

--- src/main/java/com/example/ktb/kafka/KafkaConfig.java
+++ src/main/java/com/example/ktb/kafka/KafkaConfig.java
@@ -1,8 +1,6 @@
 package com.example.ktb.kafka;
 
 import com.example.ktb.kafka.message.Message;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -12,9 +10,6 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
@@ -36,35 +31,16 @@ public class KafkaConfig {
 
     @Bean
     public ConsumerFactory<String, Message> kafkaConsumer() {
-        return new DefaultKafkaConsumerFactory<>(consumerProps(), new StringDeserializer(), new JsonDeserializer<>(Message.class, false));
+        return new DefaultKafkaConsumerFactory<>(kafkaProperties.getConsumer().buildProperties(), new StringDeserializer(), new JsonDeserializer<>(Message.class, false));
     }
 
     @Bean
     public ProducerFactory<String, Message> greetingProducerFactory() {
-        return new DefaultKafkaProducerFactory<>(senderProps(), new StringSerializer(), new JsonSerializer<>());
+        return new DefaultKafkaProducerFactory<>(kafkaProperties.getProducer().buildProperties(), new StringSerializer(), new JsonSerializer<>());
     }
 
     @Bean
     public KafkaTemplate<String, Message> greetingKafkaTemplate(ProducerFactory<String, Message> greetingProducerFactory) {
         return new KafkaTemplate<>(greetingProducerFactory);
     }
-
-    private Map<String, Object> consumerProps() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getConsumer().getBootstrapServers());
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getConsumer().getGroupId());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getConsumer().getAutoOffsetReset());
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, kafkaProperties.getConsumer().getEnableAutoCommit());
-        return props;
-    }
-
-    private Map<String, Object> senderProps() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getProducer().getBootstrapServers());
-        props.put(ProducerConfig.ACKS_CONFIG, kafkaProperties.getProducer().getAcks());
-        return props;
-    }
-
-
-
 }
